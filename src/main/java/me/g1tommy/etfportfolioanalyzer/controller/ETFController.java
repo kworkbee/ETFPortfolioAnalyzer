fdeb9e646cce3c9c65e13c5eba202379acb8c17a
G1 <g1.jeon@kakao.com>
2022-02-23 14:59:52 +0900

    feat: Context root redirection

--- src/main/java/me/g1tommy/etfportfolioanalyzer/controller/ETFController.java
+++ src/main/java/me/g1tommy/etfportfolioanalyzer/controller/ETFController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/analyzer")
 @CrossOrigin(origins = "*")
 public class ETFController {
 
